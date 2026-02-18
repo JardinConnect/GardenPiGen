ee43d5653a0f0783c72a1ee6866875593080eb0c
marius <mlehouezeclacaze@luccasoftware.com>
2026-02-18 21:36:32 +0100

    skip loggin screen

--- stage3/05-kiosk/00-run.sh
+++ stage3/05-kiosk/00-run.sh
@@ -7,6 +7,14 @@ install -m 755 files/kiosk.sh "${ROOTFS_DIR}/usr/local/bin/"
 mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart"
 
 on_chroot << EOF
-# Enable kiosk service
 systemctl enable kiosk.service
+
+loginctl enable-linger ${FIRST_USER_NAME}
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << 'AUTOLOGIN'
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin ${FIRST_USER_NAME} --noclear %I \$TERM
+AUTOLOGIN
 EOF
