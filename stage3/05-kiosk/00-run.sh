e631ad914b3072709888f5655d90b935d4c78128
Marius Le Houezec Lacaze <mlehouezeclacaze@luccasoftware.com>
2026-03-05 12:10:07 +0100

    remove system call pid 1 does not exist during build time

--- stage3/05-kiosk/00-run.sh
+++ stage3/05-kiosk/00-run.sh
@@ -6,15 +6,15 @@ install -m 755 files/kiosk.sh "${ROOTFS_DIR}/usr/local/bin/"
 
 mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart"
 
-on_chroot << EOF
-systemctl enable kiosk.service
+mkdir -p "${ROOTFS_DIR}/etc/systemd/system/graphical.target.wants"
+ln -sf ../kiosk.service "${ROOTFS_DIR}/etc/systemd/system/graphical.target.wants/kiosk.service"
 
-loginctl enable-linger ${FIRST_USER_NAME}
+mkdir -p "${ROOTFS_DIR}/var/lib/systemd/linger"
+touch "${ROOTFS_DIR}/var/lib/systemd/linger/${FIRST_USER_NAME}"
 
-mkdir -p /etc/systemd/system/getty@tty1.service.d
-cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << 'AUTOLOGIN'
+mkdir -p "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
+cat > "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf" << AUTOLOGIN
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin ${FIRST_USER_NAME} --noclear %I \$TERM
 AUTOLOGIN
-EOF
