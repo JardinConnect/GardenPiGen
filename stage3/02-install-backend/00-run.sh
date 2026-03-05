fc027c06f1ff17e5274d078b4c7aa35743d8e22c
Marius Le Houezec Lacaze <mlehouezeclacaze@luccasoftware.com>
2026-03-05 11:08:11 +0100

    update lighttpd conf again and again and gain, i am losing my mind

--- stage3/02-install-backend/00-run.sh
+++ stage3/02-install-backend/00-run.sh
@@ -2,10 +2,14 @@
 
 FRONT_SUBMODULE_PATH="${BASE_DIR}/front/GardenFront"
 
+mkdir -p "${ROOTFS_DIR}/var/www/gardenfront/web"
+
 if [ -d "${FRONT_SUBMODULE_PATH}/build" ]; then
-    mkdir -p "${ROOTFS_DIR}/var/www/gardenfront"
-    cp -r "${FRONT_SUBMODULE_PATH}/build"/* "${ROOTFS_DIR}/var/www/gardenfront/"
+    cp -r "${FRONT_SUBMODULE_PATH}/build"/* "${ROOTFS_DIR}/var/www/gardenfront/web/"
 fi
+
+rm -rf "${ROOTFS_DIR}/var/www/html"
+ln -s /var/www/gardenfront/web "${ROOTFS_DIR}/var/www/html"
 
 install -m 644 files/garden_back.service "${ROOTFS_DIR}/etc/systemd/system/"
 
@@ -21,6 +25,8 @@ install -m 644 files/.env "${ROOTFS_DIR}/opt/gardenback/"
 on_chroot << EOF
 lighty-enable-mod proxy
 lighty-enable-mod rewrite
+
+sed -i '0,/\/var\/www\/html/s//\/var\/www\/html\/web\//g' /etc/lighttpd/lighttpd.conf
 
 
 ln -sf /etc/lighttpd/conf-available/10-gardenback-proxy.conf /etc/lighttpd/conf-enabled/10-gardenback-proxy.conf
