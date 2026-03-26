8ee4a5acd6eb1afd7389f6cc8b137c9363af715a
Marius Le Houezec Lacaze <mlehouezeclacaze@luccasoftware.com>
2026-03-26 15:39:20 +0100

    remove system call pid 1 does not exist during build time

--- stage3/05-kiosk/00-run.sh
+++ stage3/05-kiosk/00-run.sh
@@ -2,14 +2,19 @@
 
 install -m 755 files/kiosk.sh "${ROOTFS_DIR}/usr/local/bin/"
 
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user"
-install -m 644 files/kiosk.service "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/"
+cat > "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bash_profile" << 'BASH_PROFILE'
+if [ -z "${SSH_TTY}" ] && [ "$(tty)" = "/dev/tty1" ]; then
+	if ! pgrep -u "${USER}" -x cage >/dev/null 2>&1; then
+		exec /usr/local/bin/kiosk.sh
+	fi
+fi
 
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/default.target.wants"
-ln -sf ../kiosk.service "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/default.target.wants/kiosk.service"
-
-mkdir -p "${ROOTFS_DIR}/var/lib/systemd/linger"
-touch "${ROOTFS_DIR}/var/lib/systemd/linger/${FIRST_USER_NAME}"
+if [ -f ~/.bashrc ]; then
+	. ~/.bashrc
+fi
+BASH_PROFILE
+chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bash_profile"
+chmod 644 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bash_profile"
 
 mkdir -p "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 cat > "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf" << AUTOLOGIN
